072c05856d271793c17a5735d4f58db10802d088
Aaron Dill <aaronsacks2006@gmail.com>
2025-03-22 17:28:20 -0500

    feat: http server

--- src/bom_retriever.py
+++ src/bom_retriever.py
@@ -3,6 +3,7 @@ import pyodbc
 from contextlib import closing
 from dataclasses import dataclass, is_dataclass, asdict
 import json
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
 
 @dataclass(kw_only=True)
@@ -59,13 +60,13 @@ class BOMRetriever:
         row = self.cursor.fetchone()
         return row is not None
 
-    def get_item_info(self, rev_ID) -> OmnifyEntry:
+    def get_item_info(self, rev_ID) -> OmnifyEntry | None:
         # Fetch the highest revision of an item with released status
         query = "SELECT Description,Status,UnderECO FROM EntryInfo WHERE Rev = ?"
         self.cursor.execute(query, (rev_ID,))
         item_row = self.cursor.fetchone()
         if not item_row:
-            return  # TODO: return http error, something went wrong
+            return None
 
         # Get revision letter matching the revision ID
         query = "SELECT Rev FROM Rev WHERE ID = ?"
@@ -79,17 +80,18 @@ class BOMRetriever:
             rev_letter=rev_letter,
         )
 
-    def load_toplevel_item(self, part_num) -> Output:
+    def load_toplevel_item(self, part_num) -> Output | (int, str):
         # Error if attempting to load an empty part number
         if not part_num:
-            # TODO: return http error, Part Number Required; please enter a part number
-            return
+            return (400, "Part Number Required: please enter a part number")
 
         # Check if any the item exists, error out if not.
         existence = self.check_item_existence(part_num)
         if not existence:
-            # TODO: return http error, Part Number Incorrect; Part number entered does not exist
-            return
+            return (
+                400,
+                "Part Number Incorrect: Part number entered does not exist",
+            )
 
         # Fetch the highest revision of an item with released status
         query = "SELECT TOP 1 Rev FROM EntryInfo WHERE (Status=1 AND PartNumber = ?) ORDER BY Rev DESC"
@@ -97,12 +99,12 @@ class BOMRetriever:
         row = self.cursor.fetchone()
         # Check if any the item is released, error out if not.
         if not row:
-            # TODO: return http error, Unreleased Item; Part number entered is not released
-            return
+            return (403, "Unreleased Item: Part number entered is not released")
         toplevel_rev_id = row[0]
 
-        # TODO: add tl_item to json output
         info = self.get_item_info(toplevel_rev_id)
+        if not info:
+            return (500, "Something went wrong")  # this should never happen
         warnings = []
 
         # After confirming item is released, check if it is currently on an open ECO.
@@ -164,23 +166,35 @@ def jsonify(o, **kwargs):
     return json.dumps(o, cls=DataclassJSONEncoder, indent=4, sort_keys=True, **kwargs)
 
 
+class Serv(BaseHTTPRequestHandler):
+    def do_GET(self):
+        part_num = self.path.split("/")[1]
+        retreiver: BOMRetriever = self.server.retreiver
+        result = retreiver.load_toplevel_item(part_num)
+        if isinstance(result, tuple):  # error case
+            self.send_response(result[0])
+            self.end_headers()
+            self.wfile.write(bytes(result[1], "utf-8"))
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(bytes(jsonify(result), "utf-8"))
+
+
 if __name__ == "__main__":
     if sys.version_info[0] < 3:
         print("Requires Python 3")
         sys.exit()
 
-    # HACK: testing
-    debug_pn = sys.argv[1]
-    print(f"Loading toplevel item for {debug_pn}")
-
+    port = sys.argv[1] or 8080
     # Open a connection to the Omnify database, create the cursor object for data retrieval
     with closing(
         pyodbc.connect(
             r"Driver=SQL Server;Server=TX01AS01;Database=omnify50;Trusted_Connection=yes;"
         )
     ) as cnxn:
-        print("Starting server")
         retreiver = BOMRetriever(cnxn)
-        result = retreiver.load_toplevel_item(debug_pn)
-        print(jsonify(result))
-        # TODO: Start server and call load_toplevel_item on request
+        print("Starting server on port", port)
+        with ThreadingHTTPServer(("localhost", port), Serv) as httpd:
+            httpd.retreiver = retreiver  # make this accessible to the request handler
+            httpd.serve_forever()
